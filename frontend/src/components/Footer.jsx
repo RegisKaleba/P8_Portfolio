25ab11a643c1e0f55360adb251be4dbb68616883
RegisKaleba <regis.kaleba@orange.fr>
2023-08-31 07:19:15 +0200

    Minor

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,15 +5,15 @@ function Footer() {
     <footer className="footer">
       <div className="contact-info">
         <div className="contact-column">
-          <p>Numéro de téléphone : +XX XXX XXX XXX</p>
-          <p>Adresse mail : example@example.com</p>
+          <p>Numéro de téléphone : 06 37 38 73 01</p>
+          <p>Adresse mail : regis.kaleba@orange.fr</p>
         </div>
       </div>
       <div className="social-links">
-        <a href="https://twitter.com/votre_nom" target="_blank" rel="noopener noreferrer">
+        <a href="@rk" target="_blank" rel="noopener noreferrer">
           Twitter
         </a>
-        <a href="https://github.com/votre_nom" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/RegisKaleba/" target="_blank" rel="noopener noreferrer">
           GitHub
         </a>
       </div>
